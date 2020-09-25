b63c6b4c8eebc906c693ce73769e7e4a5bb95319
unknown <langeraertarnaud@gmail.com>
2020-09-25 16:22:31 +0200

    Improvements to DLE. Output # of rules + only sentences with rules

--- dl_extractor/views.py
+++ dl_extractor/views.py
@@ -45,6 +45,15 @@ def make_string(inputlist):
     return ' '.join(flatlist)
 
 
+def remove_elements(remove_list, indexlist):
+    """
+    Remove a range of items from a list at once using the indexes.
+    """
+    for i in sorted(indexlist, reverse=True):
+        del remove_list[i]
+    return remove_list
+
+
 # Create your views here.
 def dle_view(request, *args, **kwargs):
     input_text   = ""
@@ -93,6 +102,16 @@ def dle_view(request, *args, **kwargs):
         then_statements = [s['then'] for s in low_info]
         else_statements = [s['else'] if s['else'] else 'None' for s in low_info]
 
+        # Only give back sentences where there actually are rules.
+        valid_list = []
+        for i in range(len(conditions)):
+            if conditions[i] == 'None':
+                valid_list.append(i)
+
+        for lst in [sentence_ids, conditions, consequences, if_statements, then_statements, else_statements, original_sentences]:
+            lst = remove_elements(lst, valid_list)
+
+        number_of_detected_rules = len(sentence_ids)
 
         sentences_info = zip(sentence_ids, original_sentences, conditions, consequences, if_statements, then_statements, else_statements)
 
@@ -104,7 +123,8 @@ def dle_view(request, *args, **kwargs):
         context = {'display_outputs': True,
                    'detail_level': detail_level,
                    'input_objects': input_objects,
-                   'sentences_info': sentences_info}
+                   'sentences_info': sentences_info,
+                   'rules_quantity': number_of_detected_rules,}
 
         # Process the text and give output
         return render(request, 'dl_extractor.html', context)
@@ -115,3 +135,46 @@ def dle_view(request, *args, **kwargs):
                    'sentences_info': []}
 
         return render(request, 'dl_extractor.html', context)
+
+
+"""
+from official_extractor.decision_logic_extractor_functions import decision_logic_extractor
+
+text = "This is an example. This is not an example if the day is Friday. This is no sentence either."
+
+answer = decision_logic_extractor(text)
+
+high_info = []
+low_info  = []
+
+original_sentences = [s[f'sentence {i+1}'] for i, s in enumerate(answer)]
+
+for s in answer:
+    if s['high'] != 'None' and s['high'] and type(s['high']) == dict:
+        high_info.extend([{'condition': make_string(s['high']['condition']),
+                           'consequence': make_string(s['high']['consequence'])}])
+    elif s['high'] == 'No conditional statements could be extracted in spite of a condition being present.':
+        high_info.extend([{'condition': 'Error during extraction',
+                           'consequence': 'Error during extraction'}])
+    else:
+       high_info.extend([{'condition':'None', 'consequence': 'None'}])
+
+    if s['low'] != 'None' and type(s['high']) == dict:
+        low_info.extend([{'if': make_string(s['low']['if']),
+                          'then': make_string(s['low']['then']),
+                          'else': make_string(s['low']['else'])}])
+    elif s['low'] == 'error during extraction':
+        low_info.extend([{'if': 'Error during extraction',
+                          'then': 'Error during extraction',
+                          'else': 'Error during extraction'}])
+    else:
+        low_info.extend([{'if':'None', 'then': 'None', 'else': 'None'}])
+
+sentence_ids     = [i+1 for i in range(len(original_sentences))]
+conditions      = [s['condition'] for s in high_info]
+consequences    = [s['consequence'] for s in high_info]
+if_statements   = [s['if'] for s in low_info]
+then_statements = [s['then'] for s in low_info]
+else_statements = [s['else'] if s['else'] else 'None' for s in low_info]
+
+"""
